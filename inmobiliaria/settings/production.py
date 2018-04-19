c28d71ba87181e283448e25d9fb5ebad27b7beb0
carlos rocha <jesus_acosta1996@hotmail.com>
2018-04-18 22:20:32 -0600

    intento 12

--- inmobiliaria/settings/production.py
+++ inmobiliaria/settings/production.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'inmobiliaria.wsgi.application'
         'PORT':6122,
     }
     }"""
-    DATABASES = {
-        'default':dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
+DATABASES = {
+    'default':dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation
